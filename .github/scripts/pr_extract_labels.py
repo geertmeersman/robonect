899725da76f72c90255e474fad0eee51e3034343
Geert Meersman <geertmeersman+github@gmail.com>
2023-05-26 09:54:30 +0200

    fix: labels and pr-checker

---
+++ .github/scripts/pr_extract_labels.py
@@ -0,0 +1,35 @@
+import re
+
+
+def extract_semver_types(commit_messages):
+    """Extract SemVer types."""
+    types = []
+    for message in commit_messages:
+        pattern = r"^(feat|fix|chore|docs|style|refactor|perf|test)(?:\(.+?\))?:\s(.+)$"
+        match = re.match(pattern, message)
+        if match and match.group(1) not in types:
+            types.append(match.group(1))
+    return types
+
+
+def get_semver_level(commit_messages):
+    """Extract SemVer level."""
+    major_keywords = ["breaking change", "major"]
+    minor_keywords = ["feat", "minor"]
+    for message in commit_messages:
+        if any(keyword in message for keyword in major_keywords):
+            return "major"
+    for message in commit_messages:
+        if any(keyword in message for keyword in minor_keywords):
+            return "minor"
+    return "patch"
+
+
+with open("COMMIT_MESSAGES", "r") as file:
+    messages = []
+    for line in file:
+        messages.append(line.strip())
+    semver_level = get_semver_level(messages)
+    types = extract_semver_types(messages)
+    types.append(semver_level)
+    print(types)
